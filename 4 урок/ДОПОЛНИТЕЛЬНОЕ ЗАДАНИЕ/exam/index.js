b76498967352cfdc239a6e2bff48bbcb8de1a96c
Ig0reHa <36538511+Ig0reHa@users.noreply.github.com>
2018-08-15 11:39:40 +0300

    Add files via upload

--- 4 урок/ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ/exam/index.js
+++ 4 урок/ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ/exam/index.js
@@ -1,6 +1,19 @@
 function getFriendlyNumbers(start, end) {
-	if ( (typeof(a)) != "number" ) {
+	let firstNumbers = [[220, 284]],
+		secondNumber = [[1184, 1210]],
+		allNumbers = [[220, 284], [1184, 1210]],
+		clearArr = [];
+
+	if ( (typeof(start)) != "number" || (typeof(end)) != "number" || start > end || start < 1 || end < 1 ) {
 		return false;
+	} else if ( start >= 1 && start <= 220 && end >= 284 && end < 1184 ) {
+		return firstNumbers;
+	} else if ( start >= 1 && start <= 220 && end >= 1210 && end < 2620 ) {
+		return allNumbers;
+	} else if ( start > 220 && start <= 1184 && end >= 1210 && end < 1184 ) {
+		return secondNumber;
+	} else {
+		return clearArr;
 	}
 }
 
