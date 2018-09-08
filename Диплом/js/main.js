16b3f8980daffb46c9a49b80842901b1c2d12e57
Ig0reHa <36538511+Ig0reHa@users.noreply.github.com>
2018-09-08 09:37:24 +0300

    Update main.js

--- Диплом/js/main.js
+++ Диплом/js/main.js
@@ -131,13 +131,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	let StylesImg = document.querySelectorAll('.styles-2'),
 		loadMoreBtn = document.getElementsByClassName('button-styles')[0];
 
-	for (let i = 0; i < StylesImg.length; i++ ) {
-		loadMoreBtn.addEventListener('click', () => {
+	loadMoreBtn.addEventListener('click', () => {
+		for (let i = 0; i < StylesImg.length; i++ ) {
 			StylesImg[i].classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
-			StylesImg[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1', 'animated', 'fadeIn');
+			StylesImg[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1', 'animated', 'zoomIn');
 			loadMoreBtn.remove();
-		});
-	}
+		}
+	});
 
 
 
